9367e9c4a376079c7de9603ec02bb0cb4c603c7a
shogi-party <shogi_party@protonmail.com>
2026-08-08 19:21:42 +0900

    Add files via upload

--- assets/notices.js
+++ assets/notices.js
@@ -1,5 +1,10 @@
 const noticesData = [
     {
+        date: "2026/08/08",
+        title: "8月22日（土）は若楠公民館で開催します。",
+        content: "先日お知らせしたとおり、8月22日（土）は若楠公民館で開催します。<br>お気をつけてお越しください🙂<br><div  align='center'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3335.58646653396!2d130.28454967568342!3d33.27731697345856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3541ca22e5397c39%3A0xeaed9b118e9d4509!2z5L2Q6LOA5biC56uL6Iul5qWg5YWs5rCR6aSo!5e0!3m2!1sja!2sjp!4v1786183607976!5m2!1sja!2sjp' width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='strict-origin-when-cross-origin'></iframe></div>"
+    },
+    {
         date: "2026/07/19",
         title: "7月・8月の活動のお知らせ（時間・場所に一部変更あり）",
         content: "7月25日（土）、8月8日（土）、8月22日（土）を予定しています。<br>一部時間と場所が変更になっていますのでご注意ください。"
